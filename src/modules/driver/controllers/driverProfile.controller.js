8cadfe5f6622c60de25e8a4e2e4b3c6fa8283d43
adityamottha <adityamottha5@gmail.com>
2026-01-13 14:06:08 +0530

    controller driver profile bug

--- src/modules/driver/controllers/driverProfile.controller.js
+++ src/modules/driver/controllers/driverProfile.controller.js
@@ -6,26 +6,26 @@ const driverProfileController = AsyncHandler(async (req,res)=>{
 
     console.log("FULLNAME: ", req.body.fullname);
     
-//     // check avatar in localPath 
-//     const driverAvatar = req.files?.avatar?.[0]?.path;
+    // check avatar in localPath 
+    const driverAvatar = req.files?.avatar?.[0]?.path;
 
-//     // set driver service 
-//     const  {driver} = await  driverProfileService({
-//     fullname:req.body.fullName,
-//     dateOfBirth:req.body.dateOfBirth,
-//     address:req.body.addess,
-//     driverAvatar,
-//     user:req.user._id
-//    });
+    // set driver service 
+    const  {driver} = await  driverProfileService({
+    fullname:req.body.fullName,
+    dateOfBirth:req.body.dateOfBirth,
+    address:req.body.addess,
+    driverAvatar,
+    user:req.user._id
+   });
 
-// //    send response 
+//    send response 
 
-// return res
-// .status(200)
-// .json(
-//     new ApiResponse(200,driver,"Driver profile completed!"
+return res
+.status(200)
+.json(
+    new ApiResponse(200,driver,"Driver profile completed!"
 
-//     ));
+    ));
 });
 
 export { driverProfileController }
