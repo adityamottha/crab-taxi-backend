b93193024c9b04ee82b673b06020b8a9845a8fd1
adityamottha <adityamottha5@gmail.com>
2026-01-15 18:55:21 +0530

    Added a parameter of driverDocuments

--- src/modules/driver/services/driverDocuments.service.js
+++ src/modules/driver/services/driverDocuments.service.js
@@ -1,7 +1,14 @@
+import { ApiError } from "../../../utils/ApiError"
 
-
-
-const driverDocumentService = async ({})=>{
+const driverDocumentService = async ({
+    user,
+    driverLicenceFiles,
+    insuranceFiles,
+    vehicleRCFiles,
+    driverLicenseCredentials,
+    insuranceCredentials,
+    vehicleRCCredentials
+})=>{
 
 }
 
