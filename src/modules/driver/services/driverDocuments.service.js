bb41d4c359a386fb07ef3d4460227b4df1b05ae1
adityamottha <adityamottha5@gmail.com>
2026-01-16 13:11:03 +0530

    Completed a driverDocumentService

--- src/modules/driver/services/driverDocuments.service.js
+++ src/modules/driver/services/driverDocuments.service.js
@@ -1,4 +1,7 @@
-import { ApiError } from "../../../utils/ApiError"
+import { ApiError } from "../../../utils/ApiError.js";
+import { DriverProfile } from ".././models/driverProfile.model.js";
+import { DriverDocuments } from ".././models/driverDocuments.model.js";
+import { uploadMultipleFiles } from "../../../utils/uploadMultipleFiles.js";
 
 const driverDocumentService = async ({
     userId,
@@ -10,13 +13,52 @@ const driverDocumentService = async ({
     vehicleRCCredentials
 })=>{
 
-    // check driver profile  existed 
-    // check driver documents ? submitted? or not ?
-    //get upload helper
-    // upload driverLicence
-    // upload insurance 
-    // upload Vehicle RC 
+    // check files fields are available 
+    if([driverLicenceFiles,insuranceFiles,vehicleRCFiles].some(fields=>!fields?.trim())){
+        throw new ApiError(404,"All files fields are required!");
+    };
 
+    // check credentials fields are available
+    if([driverLicenseCredentials,insuranceCredentials,vehicleRCCredentials].some(fields=>!fields?.trim())){
+        throw new ApiError(404,"All credentials are required!");
+    }
+
+    // check driver profile  existed 
+    const driverprofile = await DriverProfile.findOne({authUserId:userId});
+    if(!driverprofile) throw new ApiError(401,"First complete your Profile!");
+
+    // check driver documents ? submitted? or not ?
+    const existedDocs = await DriverDocuments.findById({driverProfileId:driverprofile._id});
+    if(existedDocs) throw new ApiError(409,"Documents already submitted!");
+
+    // upload files
+    const driverLicence = await uploadMultipleFiles(driverLicenceFiles);
+    const insurance = await uploadMultipleFiles(insuranceFiles);
+    const vehicleRc = await uploadMultipleFiles(vehicleRCFiles);
+
+    //inserts documents in db
+    const documents = await DriverDocuments.create({
+
+        driverProfileId:userId,
+
+        driverLicense:{
+            urls:driverLicence,
+            credentials:driverLicenseCredentials
+        },
+
+        insurance:{
+            urls:insurance,
+            credentials:insuranceCredentials
+        },
+
+        vehicleRC:{
+            urls:vehicleRc,
+            credentials:vehicleRCCredentials
+        }
+    });
+
+    // return 
+    return documents;
 }
 
 export { driverDocumentService }
