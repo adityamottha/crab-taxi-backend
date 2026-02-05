e14e9a92ed02b4ae03464967cae26aa426e88b57
adityamottha <adityamottha5@gmail.com>
2026-02-05 13:23:10 +0530

    Added a ROUTER for not-approved driver

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { verifyJWT } from "../../middlewares/authVerifyJwt.middleware.js";
 import { authorizeRole } from "../../middlewares/authorizeRole.middleware.js";
-import { getAllDriversController } from "./controllers/adminDashboard.controller.js";
+import { getAllDriversController, notApprovedDriverController } from "./controllers/adminDashboard.controller.js";
 
 const router = Router();
+
+// ALL DRIVERS ROUTE
 router.route("/drivers").get(
     verifyJWT,
     authorizeRole("ADMIN"),
     getAllDriversController
 );
+
+// NOT-APPROVED-DRIVERS ROUTE
+router.route("/not-approved").get(
+    verifyJWT,
+    authorizeRole("ADMIN"),
+    notApprovedDriverController
+)
 
 export default router;
