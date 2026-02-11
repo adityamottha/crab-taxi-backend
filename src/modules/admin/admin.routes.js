d064c6aa57e7a9b6f7976c986c69980602a14f5f
adityamottha <adityamottha5@gmail.com>
2026-02-11 11:59:29 +0530

    Completed Route for driver-documents-approval

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../../middlewares/authVerifyJwt.middleware.js";
 import { authorizeRole } from "../../middlewares/authorizeRole.middleware.js";
-import { approvedDriverProfileStatusController, getAllDriversController, notApprovedDriverController } from "./controllers/adminDashboard.controller.js";
+import { approvedDriverProfileStatusController, getAllDriversController, notApprovedDriverController, driverDocumentsApprovedController } from "./controllers/adminDashboard.controller.js";
 
 const router = Router();
 
@@ -19,10 +19,17 @@ router.route("/not-approved-drivers").get(
     notApprovedDriverController
 )
 
-// UPDATE TO APPROVED DRIVER STATUS ROUTE. 
+// UPDATE TO APPROVED DRIVER PROFILE STATUS ROUTE. 
 router.route("/driver-approved").patch(
     verifyJWT,
     authorizeRole("ADMIN"),
     approvedDriverProfileStatusController
+);
+
+// UPDATE TO APPROVED DRIVER DOCUMENTS STATUS ROUTE.
+router.route("/driver-documents-approved").patch(
+    verifyJWT,
+    authorizeRole("ADMIN"),
+    driverDocumentsApprovedController
 )
 export default router;
--- src/modules/admin/controllers/adminDashboard.controller.js
+++ src/modules/admin/controllers/adminDashboard.controller.js
@@ -41,6 +41,9 @@ const approvedDriverProfileStatusController = AsyncHandler(async (req,res)=>{
 const driverDocumentsApprovedController = AsyncHandler(async (req,res)=>{
   // call service function and pass req.body in parameter
   // send res 
+  return res.status(200).json(
+    new ApiResponse(200,{},"Driver profile Approved by admin")
+  );
 });
 
 export { 
