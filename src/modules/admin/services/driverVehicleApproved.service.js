0cb1cd5caefb364374585221a2c2d2b7a1cb62ee
adityamottha <adityamottha5@gmail.com>
2026-02-12 16:25:00 +0530

    vehicle-aproved by admin algorithm completed

---
+++ src/modules/admin/services/driverVehicleApproved.service.js
@@ -0,0 +1,13 @@
+import { Vehicle } from "../../driver/models/vehicle.model.js";
+ 
+const driverVehicleApprovedService = async ({userId})=>{
+    // check userId is not empty
+    // find user by userId
+    // check if status already approved if not next
+    // approved status
+    // update time of approval
+    // save changes
+    // return
+};
+
+export { driverVehicleApprovedService}
