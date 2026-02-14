eccca9992b41c3aa86a0dada4a8eade0846f9ae6
adityamottha <adityamottha5@gmail.com>
2026-02-14 14:07:32 +0530

    Updated vehicle model

--- src/modules/driver/models/Vehicle.model.js
+++ src/modules/driver/models/Vehicle.model.js
@@ -91,4 +91,5 @@ const vehicleSchema = new mongoose.Schema({
     },
 },{timestamps:true});
 
-export const Vehicle = mongoose.models.Vehicle || mongoose.model("Vehicle",vehicleSchema);
+export const Vehicle = mongoose.models.Vehicle || 
+mongoose.model("Vehicle",vehicleSchema);
