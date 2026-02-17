ade6a1b5d7d0dce47f6cfaded5a2f1813bee8a7b
adityamottha <adityamottha5@gmail.com>
2026-02-17 11:37:52 +0530

    Accepted current change

--- src/modules/chatRoom/controllers/chat.controller.js
+++ src/modules/chatRoom/controllers/chat.controller.js
@@ -1,28 +1,16 @@
 import { createRoomService, sendMessage, getRoomMessages } from "../services/chat.service.js";
 import { ApiResponse } from "../../../utils/ApiResponse.js";
-<<<<<<< HEAD
 import { AsyncHandler } from "../../../utils/AsyncHandler.js";
-=======
-import { AuthUser } from "../../auth/authUsers.models.js"
->>>>>>> 7c5be1b20e9856f1aa69e86c241d1ff2bd51fc92
 
 export const createRoom = AsyncHandler(async (req, res) => {
 
   const { roomType, rideId = null } = req.body;
 
-<<<<<<< HEAD
   const room = await createRoomService({
     roomType,
     rideId,
     user: req.user
   });
-=======
-  // participants built securely
-  const participants = [
-    { role: req.user.role, userId: req.user._id },
-    { role: otherUser.role, userId: _id },
-  ];
->>>>>>> 7c5be1b20e9856f1aa69e86c241d1ff2bd51fc92
 
   return res.status(200).json(
     new ApiResponse(200, room, "Room created successfully")
