29da208f277bad552ccdb20baf4e038c76c8e4c9
adityamottha <adityamottha5@gmail.com>
2026-06-12 18:43:43 +0530

    Completed createRide Controller

--- src/modules/ride matching/controllers/ride.controller.js
+++ src/modules/ride matching/controllers/ride.controller.js
@@ -1,39 +1,22 @@
-// import { Ride } from "../models/ride.model.js";
-// import { createRideService } from "../services/ride.service.js";
-// import { ApiResponse } from "../../../utils/ApiResponse.js";
-// import { AsyncHandler } from "../../../utils/AsyncHandler.js";
-// import { ApiError } from "../../../utils/ApiError.js";
+import { AsyncHandler } from "../../../utils/AsyncHandler.js";
+import { createRideService } from "../services/ride.service.js";
+import { ApiResponse } from "../../../utils/ApiResponse.js";
 
-// export const createRideController = AsyncHandler(async (req, res) => {
-//   const { pickup, drop, distance } = req.body;
+const createRideController = AsyncHandler(async (req, res) => {
 
-//   const ride = await createRideService({
-//     user: req.user,
-//     pickup,
-//     drop,
-//     distance,
-//     io: req.app.get("io"),
-//   });
+    const { pickup, dropoff } = req.body;
 
-//   return res.status(200).json(
-//     new ApiResponse(200, ride, "Ride created successfully")
-//   );
-// });
+    const ride = await createRideService({
+        passengerId: req.user._id,
+        pickup,
+        dropoff
+      });
 
-// export const acceptRideController = AsyncHandler(async (req, res) => {
-//   const { rideId, driverId } = req.body;
+    return res.status(201).json(
+        new ApiResponse(201,ride,"Ride created successfully")
+    );
+})
 
-//   const ride = await Ride.findOneAndUpdate(
-//     { _id: rideId, status: "SEARCHING" },
-//     { status: "ACCEPTED", driverId },
-//     { new: true }
-//   );
-
-//   if (!ride) {
-//     throw new ApiError(400, "Ride already accepted or not found");
-//   }
-
-//   return res.status(200).json(
-//     new ApiResponse(200, ride, "Ride accepted successfully")
-//   );
-// });
+export {
+  createRideController
+};
