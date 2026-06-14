5ab2620ffe5ecf2a0dc96711ae6a64c076b040f1
adityamottha <adityamottha5@gmail.com>
2026-06-14 12:45:48 +0530

    Completed rideServiceController

--- src/modules/ride matching/controllers/ride.controller.js
+++ src/modules/ride matching/controllers/ride.controller.js
@@ -1,5 +1,5 @@
 import { AsyncHandler } from "../../../utils/AsyncHandler.js";
-import { createRideService } from "../services/ride.service.js";
+import { createRideService, acceptRideService } from "../services/ride.service.js";
 import { ApiResponse } from "../../../utils/ApiResponse.js";
 
 const createRideController = AsyncHandler(async (req, res) => {
@@ -15,8 +15,29 @@ const createRideController = AsyncHandler(async (req, res) => {
     return res.status(201).json(
         new ApiResponse(201,ride,"Ride created successfully")
     );
-})
+});
+
+//  ACCEPT RIDE CONTROLLER
+
+const acceptRideController = (async (req, res) => {
+
+  // get rideId from params 
+    const { rideId } = req.params;
+
+    // give parameter to service function 
+    const ride = await acceptRideService({
+        rideId,
+        driverId: req.user._id
+      });
+
+      // send response 
+    return res.status(200).json(
+      new ApiResponse(200,ride,"Ride accepted successfully")
+    );
+});
+
 
 export {
-  createRideController
+  createRideController,
+  acceptRideController
 };
