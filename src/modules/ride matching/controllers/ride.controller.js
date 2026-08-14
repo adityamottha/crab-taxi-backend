e63228ac5ee04804b51831c9693c49ef19f07e30
adityamottha <adityamottha5@gmail.com>
2026-08-14 12:24:05 +0530

    Completed controller for history user/driver

--- src/modules/ride matching/controllers/ride.controller.js
+++ src/modules/ride matching/controllers/ride.controller.js
@@ -26,3 +26,63 @@ const createRideController =
   });
 
 
+
+// ==================== USER RIDE HISTORY ====================
+
+const getUserRideHistoryController = AsyncHandler(async (req, res) => {
+
+    const userId = req.user._id;
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const status = req.query.status;
+
+    const history = await getUserRideHistoryService({
+      userId,
+      page,
+      limit,
+      status,
+    });
+
+    return res.status(200).json(
+      new ApiResponse(
+      200,
+      history,
+      "User ride history fetched successfully",
+      )
+    );
+
+});
+
+
+// ==================== DRIVER RIDE HISTORY ====================
+
+const getDriverRideHistoryController = AsyncHandler(async (req, res) => {
+    const driverId = req.user._id;
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const status = req.query.status;
+
+    const history = await getDriverRideHistoryService({
+      driverId,
+      page,
+      limit,
+      status,
+    });
+
+
+    return res.status(200).json(
+      new ApiResponse(
+       200,
+       history,
+      "Driver ride history fetched successfully"
+      )
+    );
+});
+
+export {
+  createRideController,
+  getUserRideHistoryController,
+  getDriverRideHistoryController
+};
