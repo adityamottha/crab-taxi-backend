9e242e835fdc5edb82590818fe90a9f2161304b2
adityamottha <adityamottha5@gmail.com>
2026-08-25 18:00:05 +0530

    seprated daily and weekly driver earning controllers migrated to adminDravicerEarningsController

--- src/modules/admin/controllers/adminDriverEarnings.controller.js
+++ src/modules/admin/controllers/adminDriverEarnings.controller.js
@@ -1,6 +1,14 @@
 import { AsyncHandler } from "../../../utils/AsyncHandler.js";
-import { getDriverEarningsService } from "../../driver/services/driverEarnings.service.js";
 import { ApiResponse } from "../../../utils/ApiResponse";
+
+import { 
+     getDriverEarningHistoryService,
+     getDriverEarningsService, 
+     getDriverWeeklyEarningHistoryService
+     } 
+     from "../../driver/services/driverEarnings.service.js";
+
+// DRIVER EARNINGS
 const getDriverEarningsByAdminController = AsyncHandler( async (req, res) => {
 
     const driverId = req.params.driverId;
@@ -17,6 +25,50 @@ const getDriverEarningsByAdminController = AsyncHandler( async (req, res) => {
   }
 );
 
+// ============== DAILY EARNINGS HISTORY ===================
+const getDriverEarningHistoryByAdminController = AsyncHandler(async (req,res) => {
+
+  // get driver id
+  const driverId = req.params.driverId;
+
+  // call service function 
+  const history = await getDriverEarningHistoryService(driverId);
+
+  // send response 
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      history,
+      "Driver earning history fetched successfully"
+    )
+  );
+});
+
+
+// =============== WEEKLY EARNINGS CONTROLLER =============
+export const getDriverWeeklyEarningHistoryByAdminController = AsyncHandler(async (req, res) => {
+
+    // driver id from user
+      const driverId = req.params.driverId;
+
+      // get service and pass driver id
+    const history = await getDriverWeeklyEarningHistoryService(
+        driverId
+      );
+
+      // return response
+    return res.status(200).json(
+      new ApiResponse(
+        200,
+        history,
+        "Weekly earning history fetched successfully"
+      )
+    );
+  })
+
+
 export {
-    getDriverEarningsByAdminController
+    getDriverEarningsByAdminController,
+    getDriverEarningHistoryByAdminController,
+    getDriverWeeklyEarningHistoryByAdminController
 }
