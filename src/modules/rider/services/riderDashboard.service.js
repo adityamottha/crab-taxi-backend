c3bdaa0bc902019c55908106baa78897b28219aa
adityamottha <adityamottha5@gmail.com>
2026-08-29 12:50:13 +0530

    Updated getNearbyDriverService for vehicle category selection and matching to drivers

--- src/modules/rider/services/riderDashboard.service.js
+++ src/modules/rider/services/riderDashboard.service.js
@@ -2,39 +2,102 @@ import { DriverProfile } from "../../driver/models/driverProfile.model.js";
 import { ApiError } from "../../../utils/ApiError.js";
 import { AuthUser } from "../../auth/authUsers.models.js";
 import { mongoose } from "mongoose";
+import { Vehicle } from "../../driver/models/vehicle.model.js";
 
-const getNearbyDriversService = async ({ lng, lat }) => {
 
-  // validation
-  if (!lng || !lat) {
-    throw new ApiError(400, "lat and lng are required");
+const getNearbyDriversService = async ({
+  lng,
+  lat,
+  vehicleCategory,
+}) => {
+
+  // validate the data
+  if (lng === undefined || lat === undefined) {
+    throw new ApiError(
+      400,
+      "lat and lng are required"
+    );
   }
 
-  // find nearby ONLINE drivers
-  const drivers = await DriverProfile.find({
+  if (!vehicleCategory) {
+    throw new ApiError(
+      400,
+      "vehicleCategory is required"
+    );
+  }
 
+// find nearby online drivers 
+  const drivers = await DriverProfile.find({
     driverStatus: "ONLINE",
 
     location: {
       $near: {
         $geometry: {
           type: "Point",
-
-          // MongoDB format
-          coordinates: [Number(lng), Number(lat)]
+          coordinates: [
+            Number(lng),
+            Number(lat)
+          ]
         },
 
-        // 5km radius
+        // 5 KM
         $maxDistance: 5000
       }
     }
 
-  });
+  }).lean();
 
-  return drivers;
+
+  // get driver profiles id 
+  const driverProfileIds = drivers.map(
+    driver => driver._id
+  );
+
+
+
+  // find matching vehicle category 
+  const vehicles = await Vehicle.find({
+    driverProfileId: {
+      $in: driverProfileIds
+    },
+
+    vehicleCategory: vehicleCategory,
+
+    vehicleApproved: "APPROVED",
+
+    isActive: true,
+
+  }).lean();
+
+
+   
+  // get drivers who has matching vehicle 
+
+  const matchingDriverIds = new Set(
+    vehicles.map(
+      vehicle =>
+        vehicle.driverProfileId.toString()
+    )
+  );
+
+
+  
+
+//  filter drivers 
+  const matchingDrivers = drivers.filter(
+    driver =>
+      matchingDriverIds.has(
+        driver._id.toString()
+      )
+  );
+
+
+  // return 
+  return matchingDrivers;
 };
 
-// get driver profile 
+
+// GET DRIVERS PROFILE ==============================================
 const getDriverProfileForUserService = async (driverId) => {
 // console.log("driverId:", driverId);
 
