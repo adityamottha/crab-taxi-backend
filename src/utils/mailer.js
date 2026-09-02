0c04c01f41f0eeed4dc9f347ab3be2a94c897091
adityamottha <adityamottha5@gmail.com>
2026-09-02 17:43:18 +0530

    Added mail for register OTP

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,14 +1,45 @@
+import dotenv from "dotenv";
+import path from "path";
 import nodemailer from "nodemailer";
 
+dotenv.config({
+  path: path.resolve(process.cwd(), "../../.env"),
+});
+
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,   // MUST be smtp.gmail.com
+  host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // because 587
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
+
+// AUTHENTICATION VERIFICATION EMAIL --------------
+export const sendVerificationEmail = async (
+  email,
+  verifyCode
+) => {
+  await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to: email,
+    subject: "Verify your email | TanvixTechnologies",
+    html: `
+      <div>
+        <h2>Email Verification</h2>
+
+        <p>Your verification code is:</p>
+
+        <h1>${verifyCode}</h1>
+
+        <p>This code will expire in 10 minutes.</p>
+
+        <p>If you did not request this code, ignore this email.</p>
+      </div>
+    `,
+  });
+};
 
 export const sendEmail = async ({ to, subject, html }) => {
   await transporter.sendMail({
